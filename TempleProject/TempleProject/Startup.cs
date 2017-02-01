bdff3aa697272c4570bd9d79c0a0bef00001999d
Fanatos <bookzar@hotmail.com>
2017-02-01 20:39:19 +0700

    1

---
+++ TempleProject/TempleProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TempleProject.Startup))]
+namespace TempleProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
